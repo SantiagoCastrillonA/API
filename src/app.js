b560d8b3d9cec1800e5a58eebac5986ad29c4bb7
SantiagoCA <satiago.castrilloa@uqvirtual.edu.co>
2024-09-10 12:14:24 -0500

    Se arregló la creación de tokens

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ const app = express();
 app.use (morgan('dev'));
 app.use(express.json());
 
-app.use("/api", authRoutes)
+app.use("/api", authRoutes);
 
 export default app;
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,26 +1,21 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
-    const passwordHash = await bcrypt.hash(password, 10);
-
     const newUser = new User({
       username,
       email,
-      password: passwordHash,
+      password,
     });
-    const userSaved = await newUser.save();
 
-    res.json({
-      id: userSaved._id,
-      username: userSaved.username,
-      email: userSaved.email,
-    });
+    const userSave = await newUser.save();
+    res.json(userSave);
+
   } catch (error) {
     console.log(error);
   }
 };
+
 export const login = (req, res) => res.send("login");
